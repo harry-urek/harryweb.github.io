c8f2246621bf005d4718a287c00ba8c95860e1a4
Hari Om Bhardwaj <94731227+harry-urek@users.noreply.github.com>
2024-12-06 11:47:16 +0530

    chngae 1

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -9,12 +9,12 @@ const CONFIG = {
    * If you are deploying to https://<USERNAME>.github.io/<REPO_NAME>/,
    * for example your repository is at https://github.com/arifszn/portfolio, then set base to '/portfolio/'.
    */
-  base: "/gitprofile/",
+  base: "/",
   projects: {
     github: {
       display: true, // Display GitHub projects?
       header: "Github Projects",
-      mode: "automatic", // Mode can be: 'automatic' or 'manual'
+      mode: "manual", // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: "stars", // Sort projects by 'stars' or 'updated'
         limit: 6, // How many projects to display.
@@ -25,215 +25,211 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ["arifszn/gitprofile", "arifszn/pandora"], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: [
+          "harry-urek/kratos",
+          "harry-urek/ROOM",
+          "harry-urek/feynman-GPT",
+          "harry-urek/Yogi-v1",
+        ], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
-    external: {
-      header: "My Projects",
-      // To hide the `External Projects` section, keep it empty.
-      projects: [
-        {
-          title: "Project Name",
-          description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.",
-          imageUrl:
-            "https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg",
-          link: "https://example.com",
-        },
-        {
-          title: "Project Name",
-          description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.",
-          imageUrl:
-            "https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg",
-          link: "https://example.com",
-        },
-      ],
+    //   external: {
+    //     header: "My Projects",
+    //     // To hide the `External Projects` section, keep it empty.
+    //     projects: [
+    //       {
+    //         title: "Project Name",
+    //         description:
+    //           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.",
+    //         imageUrl:
+    //           "https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg",
+    //         link: "https://example.com",
+    //       },
+    //       {
+    //         title: "Project Name",
+    //         description:
+    //           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.",
+    //         imageUrl:
+    //           "https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg",
+    //         link: "https://example.com",
+    //       },
+    //     ],
+    //   },
+    // },
+    seo: {
+      title: "Portfolio of Hari Om Bhardwaj",
+      description: "Backend_developer , Machine Learning Engineer , ",
+      imageURL: "",
     },
-  },
-  seo: {
-    title: "Portfolio of Hari Om Bhardwaj",
-    description: "Backend_developer , Machine Learning Engineer , ",
-    imageURL: "",
-  },
-  social: {
-    linkedin: "ariful-alam",
-    twitter: "arif_szn",
-    mastodon: "arifszn@mastodon.social",
-    researchGate: "",
-    facebook: "",
-    instagram: "",
-    reddit: "",
-    threads: "",
-    youtube: "", // example: 'pewdiepie'
-    udemy: "",
-    dribbble: "",
-    behance: "",
-    medium: "arifszn",
-    dev: "arifszn",
-    stackoverflow: "", // example: '1/jeff-atwood'
-    skype: "",
-    telegram: "",
-    website: "https://www.arifszn.com",
-    phone: "",
-    email: "arifulalamszn@gmail.com",
-  },
-  resume: {
-    fileUrl:
-      "https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf", // Empty fileUrl will hide the `Download Resume` button.
-  },
-  skills: [
-    "PHP",
-    "Laravel",
-    "JavaScript",
-    "React.js",
-    "Node.js",
-    "Nest.js",
-    "MySQL",
-    "PostgreSQL",
-    "Git",
-    "Docker",
-    "PHPUnit",
-    "CSS",
-    "Antd",
-    "Tailwind",
-  ],
-  experiences: [
-    {
-      company: "Company Name",
-      position: "Position",
-      from: "September 2021",
-      to: "Present",
-      companyLink: "https://example.com",
+    social: {
+      linkedin: "ariful-alam",
+      twitter: "arif_szn",
+      mastodon: "arifszn@mastodon.social",
+      researchGate: "",
+      facebook: "",
+      instagram: "",
+      // reddit: "",
+      // threads: "",
+      // youtube: "", // example: 'pewdiepie'
+      // udemy: "",
+      // dribbble: "",
+      // behance: "",
+      // medium: "arifszn",
+      // dev: "arifszn",
+      // stackoverflow: "", // example: '1/jeff-atwood'
+      // skype: "",
+      // telegram: "",
+      website: "https://www.arifszn.com",
+      phone: "",
+      email: "hariombhardwaj038@gmail.com",
     },
-    {
-      company: "Company Name",
-      position: "Position",
-      from: "July 2019",
-      to: "August 2021",
-      companyLink: "https://example.com",
+    resume: {
+      fileUrl:
+        "https://drive.google.com/file/d/1FGyGvDyqlhR8kn_YflT30-bfnNWcL6J_/view?usp=sharing", // Empty fileUrl will hide the `Download Resume` button.
     },
-  ],
-  certifications: [
-    {
-      name: "Lorem ipsum",
-      body: "Lorem ipsum dolor sit amet",
-      year: "March 2022",
-      link: "https://example.com",
-    },
-  ],
-  educations: [
-    {
-      institution: "Institution Name",
-      degree: "Degree",
-      from: "2015",
-      to: "2019",
-    },
-    {
-      institution: "Institution Name",
-      degree: "Degree",
-      from: "2012",
-      to: "2014",
-    },
-  ],
-  publications: [
-    {
-      title: "Publication Title",
-      conferenceName: "",
-      journalName: "Journal Name",
-      authors: "John Doe, Jane Smith",
-      link: "https://example.com",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      title: "Publication Title",
-      conferenceName: "Conference Name",
-      journalName: "",
-      authors: "John Doe, Jane Smith",
-      link: "https://example.com",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-  ],
-  // Display articles from your medium or dev account. (Optional)
-  blog: {
-    source: "", // medium | dev
-    username: "arifszn", // to hide blog section, keep it empty
-    limit: 2, // How many articles to display. Max is 10.
-  },
-  googleAnalytics: {
-    id: "", // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
-  },
-  // Track visitor interaction and behavior. https://www.hotjar.com
-  hotjar: {
-    id: "",
-    snippetVersion: 6,
-  },
-  themeConfig: {
-    defaultTheme: "Procyon",
-
-    // Hides the switch in the navbar
-    // Useful if you want to support a single color mode
-    disableSwitch: false,
-
-    // Should use the prefers-color-scheme media-query,
-    // using user system preferences, instead of the hardcoded defaultTheme
-    respectPrefersColorScheme: false,
-
-    // Display the ring in Profile picture
-    displayAvatarRing: true,
-
-    // Available themes. To remove any theme, exclude from here.
-    themes: [
-      "light",
-      "dark",
-      "cupcake",
-      "bumblebee",
-      "emerald",
-      "corporate",
-      "synthwave",
-      "retro",
-      "cyberpunk",
-      "valentine",
-      "halloween",
-      "garden",
-      "forest",
-      "aqua",
-      "lofi",
-      "pastel",
-      "fantasy",
-      "wireframe",
-      "black",
-      "luxury",
-      "dracula",
-      "cmyk",
-      "autumn",
-      "business",
-      "acid",
-      "lemonade",
-      "night",
-      "coffee",
-      "winter",
-      "dim",
-      "nord",
-      "sunset",
-      "procyon",
+    skills: [
+      "Python",
+      "Go",
+      "Java",
+      "Pandas",
+      "CI/CD",
+      "PyTorch",
+      "Data Modeling",
+      "API developer",
+      "ML",
+      "GenAI",
+      "MySQL",
+      "PostgreSQL",
+      "Git",
+      "Docker",
+      "Nvim",
+      "FastAPI",
+      "Django",
+      "Authentication",
+    ],
+    experiences: [
+      {
+        company: "Gradio",
+        position: "Maintainer",
+        from: "Oct 2023",
+        to: "Nov 2024",
+        companyLink: "https://www.gradio.app/",
+      },
     ],
 
-    // Custom theme, applied to `procyon` theme
-    customTheme: {
-      primary: "#fc055b",
-      secondary: "#219aaf",
-      accent: "#e8d03a",
-      neutral: "#2A2730",
-      "base-100": "#E3E3ED",
-      "--rounded-box": "3rem",
-      "--rounded-btn": "3rem",
+    educations: [
+      {
+        institution: "IIT Madras",
+        degree: "BS Data Science",
+        from: "2023",
+        to: "2028",
+      },
+      {
+        institution: "Siskha 'O' Anusandhan ITER College ",
+        degree: "B.Tech Computer Science",
+        from: "2021",
+        to: "2025",
+      },
+    ],
+    publications: [
+      {
+        title: "Publication Title",
+        conferenceName: "",
+        journalName: "Journal Name",
+        authors: "John Doe, Jane Smith",
+        link: "https://example.com",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      },
+      {
+        title: "Publication Title",
+        conferenceName: "Conference Name",
+        journalName: "",
+        authors: "John Doe, Jane Smith",
+        link: "https://example.com",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+      },
+    ],
+    // Display articles from your medium or dev account. (Optional)
+    blog: {
+      source: "", // medium | dev
+      username: "arifszn", // to hide blog section, keep it empty
+      limit: 2, // How many articles to display. Max is 10.
     },
-  },
+    googleAnalytics: {
+      id: "", // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    },
+    // Track visitor interaction and behavior. https://www.hotjar.com
+    hotjar: {
+      id: "",
+      snippetVersion: 6,
+    },
+    themeConfig: {
+      defaultTheme: "Procyon",
 
-  enablePWA: true,
+      // Hides the switch in the navbar
+      // Useful if you want to support a single color mode
+      disableSwitch: false,
+
+      // Should use the prefers-color-scheme media-query,
+      // using user system preferences, instead of the hardcoded defaultTheme
+      respectPrefersColorScheme: false,
+
+      // Display the ring in Profile picture
+      displayAvatarRing: true,
+
+      // Available themes. To remove any theme, exclude from here.
+      themes: [
+        "light",
+        "dark",
+        "cupcake",
+        "bumblebee",
+        "emerald",
+        "corporate",
+        "synthwave",
+        "retro",
+        "cyberpunk",
+        "valentine",
+        "halloween",
+        "garden",
+        "forest",
+        "aqua",
+        "lofi",
+        "pastel",
+        "fantasy",
+        "wireframe",
+        "black",
+        "luxury",
+        "dracula",
+        "cmyk",
+        "autumn",
+        "business",
+        "acid",
+        "lemonade",
+        "night",
+        "coffee",
+        "winter",
+        "dim",
+        "nord",
+        "sunset",
+        "procyon",
+      ],
+
+      // Custom theme, applied to `procyon` theme
+      customTheme: {
+        primary: "#fc055b",
+        secondary: "#219aaf",
+        accent: "#e8d03a",
+        neutral: "#2A2730",
+        "base-100": "#E3E3ED",
+        "--rounded-box": "3rem",
+        "--rounded-btn": "3rem",
+      },
+    },
+
+    enablePWA: true,
+  },
 };
 
 export default CONFIG;
