2efe2c279b5dddfe9adbfe48e10678cf4d299d08
Hari Om Bhardwaj <94731227+harry-urek@users.noreply.github.com>
2024-12-06 11:56:33 +0530

    daaa

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -33,28 +33,28 @@ const CONFIG = {
         ], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
-    external: {
-      header: "My Projects",
-      // To hide the `External Projects` section, keep it empty.
-      projects: [
-        {
-          title: "Project Name",
-          description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.",
-          imageUrl:
-            "https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg",
-          link: "https://example.com",
-        },
-        {
-          title: "Project Name",
-          description:
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.",
-          imageUrl:
-            "https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg",
-          link: "https://example.com",
-        },
-      ],
-    },
+    // external: {
+    //   header: "My Projects",
+    //   // To hide the `External Projects` section, keep it empty.
+    //   projects: [
+    //     {
+    //       title: "Project Name",
+    //       description:
+    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.",
+    //       imageUrl:
+    //         "https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg",
+    //       link: "https://example.com",
+    //     },
+    //     {
+    //       title: "Project Name",
+    //       description:
+    //         "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.",
+    //       imageUrl:
+    //         "https://img.freepik.com/free-vector/illustration-gallery-icon_53876-27002.jpg",
+    //       link: "https://example.com",
+    //     },
+    //   ],
+    // },
   },
   seo: {
     title: "Portfolio of Hari Om Bhardwaj",
@@ -131,30 +131,30 @@ const CONFIG = {
       to: "2025",
     },
   ],
-  publications: [
-    {
-      title: "Publication Title",
-      conferenceName: "",
-      journalName: "Journal Name",
-      authors: "John Doe, Jane Smith",
-      link: "https://example.com",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-    {
-      title: "Publication Title",
-      conferenceName: "Conference Name",
-      journalName: "",
-      authors: "John Doe, Jane Smith",
-      link: "https://example.com",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    },
-  ],
+  // publications: [
+  //   {
+  //     title: "Publication Title",
+  //     conferenceName: "",
+  //     journalName: "Journal Name",
+  //     authors: "John Doe, Jane Smith",
+  //     link: "https://example.com",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  //   },
+  //   {
+  //     title: "Publication Title",
+  //     conferenceName: "Conference Name",
+  //     journalName: "",
+  //     authors: "John Doe, Jane Smith",
+  //     link: "https://example.com",
+  //     description:
+  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+  //   },
+  // ],
   // Display articles from your medium or dev account. (Optional)
   blog: {
-    source: "", // medium | dev
-    username: "arifszn", // to hide blog section, keep it empty
+    source: "hashnode", // medium | dev
+    username: "NoobGramar", // to hide blog section, keep it empty
     limit: 2, // How many articles to display. Max is 10.
   },
   googleAnalytics: {
